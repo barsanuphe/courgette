ea1f5c5a4f1bbe05c492e3867bc05b95045526a4
Barsanuphe <paixetprosperite@gmail.com>
2016-01-29 00:07:11 +0100

    Added Collection.Analyze().

--- courgette/collection.go
+++ courgette/collection.go
@@ -2,6 +2,8 @@
 // Useful if you organize your picture with a different folder for each month.
 package courgette
 
+import "errors"
+
 // Collection represents a collection of Pictures
 type Collection struct {
 	Config
@@ -23,9 +25,13 @@ func (c *Collection) SortNew() (numSorted int, err error) {
 
 // AnalyzeContents of a given subdirectory.
 func (c *Collection) AnalyzeContents(subdir string) (numFiles int, err error) {
-	// TODO find SubDirectory
-	// TODO s.Analyze()
-	return
+	for _, subd := range c.Contents {
+		if subd == subdir {
+			numFiles, err = subd.Analyze()
+			return
+		}
+	}
+	return 0, errors.New("Subdir " + subdir + " not found")
 }
 
 // Refresh filenames in a given subdirectory.
