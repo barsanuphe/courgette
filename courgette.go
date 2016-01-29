fb4e4bd0fa188eadf41ca5248ba114a5ae25a8cd
Barsanuphe <paixetprosperite@gmail.com>
2016-01-30 00:01:57 +0100

    Improved cli interface.

--- courgette.go
+++ courgette.go
@@ -29,47 +29,64 @@ func main() {
 
 	app.Commands = []cli.Command{
 		{
-			Name:    "import",
-			Aliases: []string{"i"},
-			Usage:   "import from card reader.",
+			Name:        "import",
+			Aliases:     []string{"i"},
+			Usage:       "import pictures from card reader or sort incoming",
+			ArgsUsage:   "[DISK_NAME]",
+			Description: "import from card reader if DISK_NAME is provided, or incoming directory.",
+			Action: func(c *cli.Context) {
+				// TODO if args, import from disk with that name
+				// TODO if not, or after copying from disk, sort files in incoming
+			},
+		},
+		{
+			Name:        "export",
+			Aliases:     []string{"x"},
+			Usage:       "export pictures with a given tag to a folder.",
+			ArgsUsage:   "TAG FOLDER",
+			Description: "export pictures with a TAG to a local directory.",
 			Action: func(c *cli.Context) {
 				// TODO
 			},
 		},
 		{
-			Name:    "export",
-			Aliases: []string{"x"},
-			Usage:   "export [tag] to [folder].",
+			Name:        "findorphans",
+			Aliases:     []string{"f"},
+			Usage:       "find raw files without jpg versions, to be removed.",
+			ArgsUsage:   "[SUBDIR]",
+			Description: "Raw pictures without jpg versions are flagged for deletion.\n   If SUBDIR is given, only this subdirectory is considered instead of the whole collection.",
 			Action: func(c *cli.Context) {
 				// TODO
 			},
 		},
 		{
-			Name:    "collection",
-			Aliases: []string{"c"},
-			Usage:   "options for photo collection",
-			Subcommands: []cli.Command{
-				{
-					Name:  "findorphans",
-					Usage: "find orphan raw files in a subdirectory.",
-					Action: func(c *cli.Context) {
-						// TODO
-					},
-				},
-				{
-					Name:  "sortnew",
-					Usage: "sort newly imported pictures.",
-					Action: func(c *cli.Context) {
-						// TODO
-					},
-				},
-				{
-					Name:  "refresh",
-					Usage: "refresh filenames in a subdirectory.",
-					Action: func(c *cli.Context) {
-						// TODO
-					},
-				},
+			Name:        "refresh",
+			Aliases:     []string{"r"},
+			ArgsUsage:   "[SUBDIR]",
+			Usage:       "refresh filenames in a subdirectory.",
+			Description: "Rename pictures according to configuration and metadata, after import.\n   If SUBDIR is given, only this subdirectory is considered instead of the whole collection.",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:        "diff",
+			Aliases:     []string{"d"},
+			ArgsUsage:   "[SUBDIR]",
+			Usage:       "show changes relative to last commit.",
+			Description: "Show changes since last commit.\n   If SUBDIR is given, only this subdirectory is considered instead of the whole collection.",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:        "commit",
+			Aliases:     []string{"c"},
+			ArgsUsage:   "[SUBDIR]",
+			Usage:       "accept changes in the collection.",
+			Description: "Accept changes in the collection and save the current state.\n   If SUBDIR is given, only this subdirectory is considered instead of the whole collection.",
+			Action: func(c *cli.Context) {
+				// TODO save hash and info
 			},
 		},
 	}
